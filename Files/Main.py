4fadadd30ca952ff434ffb3cd4927e1165bc375f
jaroslaw <jarek.skrzypczak@o2.pl>
2017-11-21 21:16:17 +0100

    Add calc avg RGB colors

--- Files/Main.py
+++ Files/Main.py
@@ -12,10 +12,23 @@ def writeBitmapToFile(bitmap, fileName):
     image.save(path)
 
 
+def calculateAveragesRGBColor(bitmap):
+    rSum, gSum, bSum = 0, 0, 0
+    for row in bitmap:
+        for column in row:
+            rSum += column[0]
+            gSum += column[1]
+            bSum += column[2]
+    pixelsCount = len(bitmap) * len(bitmap[0])
+    return [rSum / pixelsCount, gSum / pixelsCount, bSum / pixelsCount]
+
+
 def main():
     fileName = "JGC0.jpg"
     print("Photos/" + fileName)
     bitmap = readBitmapFromFile("Photos/" + fileName)
+    print(calculateAveragesRGBColor(bitmap))
+    print(np.std(bitmap))
     writeBitmapToFile(bitmap, fileName)
 
 
