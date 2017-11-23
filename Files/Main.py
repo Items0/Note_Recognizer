afafb7082aab8387320707459cbf20348290200e
jaroslaw <jarek.skrzypczak@o2.pl>
2017-11-23 08:39:59 +0100

    Add createBitmapWithMask Add a few mask Now algorithm detect edge and denoise image

--- Files/Main.py
+++ Files/Main.py
@@ -1,20 +1,28 @@
 import numpy as np
 from PIL import Image as im
-import PIL.ImageStat as imageStat # fajna clasa analizująca  imageStat.Stat(Image)._get[co chcę (mean, stddev ...)]
+import PIL.ImageStat as imageStat  # fajna clasa analizująca  imageStat.Stat(Image)._get[co chcę (mean, stddev ...)]
 import matplotlib.pyplot as plt
+from skimage.filters.edges import convolve
 
 MULTIPLE_STD_PARAM = 2.0
-FILE_SUFIX = "std2.0"
+FILE_SUFIX = "edgeLap2withMean9"
+MASK_EDGE_HORIZONTAL = np.array([[1, 2, 1],
+                                 [0, 0, 0],
+                                 [-1, -2, -1]]) / 8
+MASK_EDGE_VERTICAL = np.array([[1, 0, -1],
+                               [2, 0, -2],
+                               [1, 0, -1]]) / 8
+MASK_EDGE_LAPLACE = np.array([[-1, -1, -1],
+                              [-1, 8, -1],
+                              [-1, -1, -1]]) / 16
+MASK_MEAN = np.array([[1,1,1],
+                      [1,1,1],
+                      [1,1,1]])/9
 
 def readBitmapFromFile(fileName):
     path = "Photos/" + fileName + ".jpg"
     image = im.open(path)
-    print(type(image))
-    image = image.resize((int(image.size[0]/np.sqrt(6)), int(image.size[1]/np.sqrt(6))))
-    print(imageStat.Stat(image)._getvar())
-    plt.plot(image.histogram())
-    plt.show()
-    print(image.histogram())
+    image = image.resize((int(image.size[0] / np.sqrt(6)), int(image.size[1] / np.sqrt(6))))
     return np.asarray(image)
 
 
@@ -42,7 +50,7 @@ def thresholding(bitmap, avegaresRGB, std=0):
     gThresh = avegaresRGB[1] - std if avegaresRGB[1] - std > 0 else 0
     bThresh = avegaresRGB[2] - std if avegaresRGB[2] - std > 0 else 0
     print(avegaresRGB)
-    print(rThresh," ", gThresh," ", bThresh)
+    print(rThresh, " ", gThresh, " ", bThresh)
     print(std)
     for row in bitmap:
         threshRow = []
@@ -55,9 +63,23 @@ def thresholding(bitmap, avegaresRGB, std=0):
     return threshBitmap
 
 
+def createBitmapWithMask(bitmap, mask):
+    return np.abs(convolve(bitmap, mask[:, :, None]))
+
+
+def edgeDetect(bitmap):
+    bitmap = createBitmapWithMask(bitmap,MASK_MEAN)
+    bitmap = createBitmapWithMask(bitmap,MASK_EDGE_LAPLACE)
+    #bitmapHor = createBitmapWithMask(bitmap, MASK_EDGE_VERTICAL)
+    #bitmapVer = createBitmapWithMask(bitmap, MASK_EDGE_VERTICAL)
+    #bitmap = (bitmapHor + bitmapVer) / 2
+    return bitmap
+
+
 def makeImage(fileName):
     bitmap = readBitmapFromFile(fileName)
-    bitmap = thresholding(bitmap, calculateAveragesRGBColor(bitmap), np.std(bitmap))
+    bitmap = edgeDetect(bitmap)
+    # bitmap = thresholding(bitmap, calculateAveragesRGBColor(bitmap), np.std(bitmap))
     writeBitmapToFile(bitmap, fileName)
 
 
